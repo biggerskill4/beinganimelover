f4166b9823950c470453daee3990aeaa6c92f1db
Subhan Ahmed <subhanahmede860@gmail.com>
2026-03-07 14:28:34 +0500

    Fix some responsive issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,8 +20,8 @@ const triggerAnime = getRandomAnimeName();
 const heroSlider = document.getElementById("hero-slider");
 
 // Find the anime object
-const animeToShow = animeHeroList.find(a => a.animeName === triggerAnime);
-// const animeToShow = animeHeroList.find(a => a.animeName === "Demon Slayer");
+// const animeToShow = animeHeroList.find(a => a.animeName === triggerAnime);
+const animeToShow = animeHeroList.find(a => a.animeName === "Attack on Titan");
 
 // Create slides for each character
 animeToShow.characters.forEach(char => {
